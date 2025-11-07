3bdf0c215a70c5595a83f7509dd6b1f14e018378
elplak <elplak19@gmail.com>
2025-11-07 18:52:16 +0100

    add line and column tracking to tokens for better error reporting
    :wq
    :wq
    :wq

--- AOTInterpreter/Lexer.cs
+++ AOTInterpreter/Lexer.cs
@@ -4,6 +4,7 @@ public enum TokenType
 {
     Identifier,
     Number,
+    String,
     Plus,
     Minus,
     Star,
@@ -15,58 +16,135 @@ public enum TokenType
     EOF
 }
 
-public record Token(TokenType Type, string Text);
+// adds ToString override so tokens display their position when printed or logged --> especially useful for error messages TODO: unit tests should cover this
+public record Token(TokenType Type, string Text, int Line, int Column)
+{
+    public override string ToString()
+    {
+        return $"{Type} ('{Text}') at line {Line}, column {Column}";
+    }
+}
 
 public class Lexer(string text)
 {
     private int _pos;
+    private int _line = 1;
+    private int _col = 1;
 
     public Token NextToken()
     {
         while (_pos < text.Length)
         {
-            char c = text[_pos];
+            var c = text[_pos];
+
+            // skip whitespaces for now
             if (char.IsWhiteSpace(c))
             {
+                if (c == '\n')
+                {
+                    _line++;
+                    _col = 1;
+                }
+                else
+                {
+                    _col++;
+                }
+
                 _pos++;
                 continue;
             }
 
-            if (char.IsLetter(c)) return ReadIdentifier();
-            if (char.IsDigit(c)) return ReadNumber();
+            if (char.IsLetter(c))
+            {
+                return ReadIdentifier();
+            }
 
+            if (char.IsDigit(c))
+            {
+                return ReadNumber();
+            }
+
+            if (c == '"')
+            {
+                return ReadString();
+            }
+
+            var startCol = _col;
             _pos++;
+            _col++;
             return c switch
             {
-                '+' => new(TokenType.Plus, "+"),
-                '-' => new(TokenType.Minus, "-"),
-                '*' => new(TokenType.Star, "*"),
-                '/' => new(TokenType.Slash, "/"),
-                '=' => new(TokenType.Equal, "="),
-                '(' => new(TokenType.LParen, "("),
-                ')' => new(TokenType.RParen, ")"),
-                _ => throw new Exception($"Unexpected character: {c}")
+                '+' => new Token(TokenType.Plus, "+", _line, startCol),
+                '-' => new Token(TokenType.Minus, "-", _line, startCol),
+                '*' => new Token(TokenType.Star, "*", _line, startCol),
+                '/' => new Token(TokenType.Slash, "/", _line, startCol),
+                '=' => new Token(TokenType.Equal, "=", _line, startCol),
+                '(' => new Token(TokenType.LParen, "(", _line, startCol),
+                ')' => new Token(TokenType.RParen, ")", _line, startCol),
+                _ => throw new Exception($"Unexpected character '{c}' at line {_line}, column {startCol}"),
             };
         }
 
-        return new Token(TokenType.EOF, "");
+        return new Token(TokenType.EOF, "", _line, _col);
     }
 
     private Token ReadIdentifier()
     {
         var start = _pos;
+        var startCol = _col;
         while (_pos < text.Length && char.IsLetter(text[_pos]))
         {
             _pos++;
+            _col++;
         }
+
         var id = text[start.._pos];
-        return id == "print" ? new Token(TokenType.Print, id) : new Token(TokenType.Identifier, id);
+        if (id == "print")
+        {
+            return new Token(TokenType.Print, id, _line, startCol);
+        }
+        return new Token(TokenType.Identifier, id, _line, startCol);
     }
 
     private Token ReadNumber()
     {
         var start = _pos;
-        while (_pos < text.Length && char.IsDigit(text[_pos])) _pos++;
-        return new Token(TokenType.Number, text[start.._pos]);
+        var startCol = _col;
+        while (_pos < text.Length && char.IsDigit(text[_pos]))
+        {
+            _pos++;
+            _col++;
+        }
+
+        return new Token(TokenType.Number, text[start.._pos], _line, startCol);
+    }
+
+    private Token ReadString()
+    {
+        var startCol = _col;
+        _pos++; // skip opening "
+        _col++;
+
+        var start = _pos;
+        while (_pos < text.Length && text[_pos] != '"')
+        {
+            if (text[_pos] == '\n')
+            {
+                throw new Exception($"Unterminated string starting at line {_line}, column {startCol}");
+            }
+            _pos++;
+            _col++;
+        }
+
+        if (_pos >= text.Length)
+        {
+            throw new Exception($"Unterminated string starting at line {_line}, column {startCol}");
+        }
+
+        var value = text[start.._pos];
+        _pos++; // skip closing "
+        _col++;
+
+        return new Token(TokenType.String, value, _line, startCol);
     }
 }
